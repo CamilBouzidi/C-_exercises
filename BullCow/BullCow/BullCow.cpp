d39adc016e85aeb5b63144c6b5cdafc0d7d70c01
CamilBouzidi <45476178+CamilBouzidi@users.noreply.github.com>
2019-05-09 18:14:46 -0400

    Almost done with BullNCow

--- BullCow/BullCow/BullCow.cpp
+++ BullCow/BullCow/BullCow.cpp
@@ -1,17 +1,39 @@
 //a hash # is a pre-processor directive. Library code in <>.
 //Tutorial is a courtesy of Ben Tristem, Udemy.
 #include <iostream>
-//#include <string> This seems to be outdated.
+#include <string>
 using namespace std;
 
 int main()
 {
 	//introduce the game
 	constexpr int WORD_LENGTH = 5;
+	string iso = "carol";
 	cout << "Welcome to The Bulls and Cows Game\n";
-	cout << "Try finding the"<< WORD_LENGTH <<"-letter isogram I'm thinking of! Enter it now: \n";
+	cout << "Try finding the"<< WORD_LENGTH <<"-letter isogram I'm thinking of!";
 	//Get guess from player and repeat it 
 	string guess;
-	cin >> guess;
-	cout << "Your guess was: " << guess;
+	bool found = false;
+	int ctr = 0;
+	int bull = 0;
+	int cow = 0;
+	do {
+		cout << "Enter your guess:\n";
+		getline(cin, guess);
+		ctr++;
+		cout << "Your guess was: " << guess;
+		for (int i = 0; i < iso.length(); i++) {
+			if (guess.at(i) == iso.at(i))
+				bull++;
+			else if (guess.find(iso.at(0)) != -1)
+				cow++;
+		}
+		cout << "Bulls " << bull << " Cows " << cow;
+	} while ((ctr<10)||(!found));
+	if (found)
+		cout << "You won!";
+	else
+		cout << "You lost! The isogram was" << iso;
+	return 0;
+
 }
